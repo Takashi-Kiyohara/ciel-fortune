3a0112eb7414ec4d411a134df8906607504fb27f
Takashi-Kiyohara <178084327+Takashi-Kiyohara@users.noreply.github.com>
2026-03-26 23:46:47 +0900

    LP final version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,15 @@
-import HeroSection from "@/components/HeroSection";
-import Separator from "@/components/Separator";
-import FortuneCollection from "@/components/FortuneCollection";
-import MisaSection from "@/components/MisaSection";
-import PricingSection from "@/components/PricingSection";
-import FAQSection from "@/components/FAQSection";
-import Footer from "@/components/Footer";
-
 export default function Home() {
   return (
-    <>
-      <HeroSection />
-      <Separator />
-      <FortuneCollection />
-      <Separator />
-      <MisaSection />
-      <Separator />
-      <PricingSection />
-      <Separator />
-      <FAQSection />
-      <Footer />
-    </>
+    <iframe
+      src="/prototype.html"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        border: 'none',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+      }}
+    />
   );
 }
